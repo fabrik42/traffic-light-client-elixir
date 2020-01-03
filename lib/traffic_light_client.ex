0766209762a992c93e20acdc0366c235a49dec32
Christian Bäuerlein <fabrik42@gmail.com>
2020-01-03 11:05:48 +0100

    Remove unused boilerplate

--- lib/traffic_light_client.ex
+++ lib/traffic_light_client.ex
@@ -2,17 +2,4 @@ defmodule TrafficLightClient do
   @moduledoc """
   Documentation for TrafficLightClient.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TrafficLightClient.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/traffic_light_client_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TrafficLightClientTest do
-  use ExUnit.Case
-  doctest TrafficLightClient
-
-  test "greets the world" do
-    assert TrafficLightClient.hello() == :world
-  end
-end
